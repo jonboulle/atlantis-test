3198ed08f761f325bc27c935292f6513b391967d
Jonathan Boulle <jonathanboulle@gmail.com>
2019-05-29 12:23:31 +0200

    test removing data source

--- main.tf
+++ main.tf
@@ -1,9 +1,1 @@
-data "archive_file" "empty_lambda_initialiser" {
-  type        = "zip"
-  output_path = "${path.module}/.terraform/empty_lambda_initialiser.zip"
 
-  source {
-    content  = "This is a dummy file to initialise a Lambda function."
-    filename = "README.txt"
-  }
-}
